465a25691db8287ec60e841ae9e29661d43a17d6
BumbleBPuffin953 <antoniodimino1@gmail.com>
2025-05-28 14:39:19 -0400

    corrupt soil sc3k

--- functions.py
+++ functions.py
@@ -188,7 +188,7 @@ def minion_processing(minions, fuels, upgrades, bazaar_cache,misc_upgrades):
                     continue
                 if not all(is_compatible(minion, u) for u in (up1, up2)):
                     continue
-                if key1 == "CORRUPT_SOIL" and key2 == 'SUPER_COMPACTOR_3000':
+                if {key1, key2} == {"CORRUPT_SOIL", "SUPER_COMPACTOR_3000"}:
                     continue
 
                 bazaar = True if up1.get("Name") == "Super Compactor" or up2.get("Name") == "Super Compactor" else False
